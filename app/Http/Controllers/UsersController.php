71280e9bc90258c0196a22111b01969f3addd5c7
Henrique Siqueira <h.enrique.siq@hotmail.com>
2022-11-27 09:17:34 -0300

    Wip

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -10,37 +11,53 @@ class UsersController extends Controller
     {
     }
 
-    public function index(Request $request)
+    public function login(Request $request)
     {
-        return '123123123123';
+        $verify = User::query()
+            ->where('email', '=', $request['email'])
+            ->where('password', '=', $request['password'])
+            ->first();
 
+        if (isset($verify)) {
+            return [
+                'message' => 'Usuário logado com sucesso',
+                'status' => 'success',
+                'data' => json_encode($verify)
+            ];
+        } else {
+            return [
+                'message' => 'Email ou senha incorretos, favor tentar novamente',
+                'status' => 'error'
+            ];
+        }
 
     }
 
-
-    public function store(Request $request)
+    public function register(Request $request)
     {
-        return '1231231111111111111111111111111123123';
+        $verify = User::query()->where('email', '=', $request['email'])->first();
+
+        if (isset($verify)) {
+            return [
+                'message' => 'Esse email já encontra cadastrado na plataforma',
+                'status' => 'error'
+            ];
+        }
+
+        $user = new User();
+        $user->name = $request['name'];
+        $user->cpf = $request['cpf'];
+        $user->telefone = $request['telefone'];
+        $user->email = $request['email'];
+        $user->password = $request['password'];
+        $user->type = $request['selectedType'];
+        $user->save();
+
+        return [
+            'message' => 'Usuário cadastrado com sucesso',
+            'status' => 'success'
+        ];
 
     }
 
-
-    public function show(Request $request)
-    {
-
-
-    }
-
-
-    public function update( Request $request)
-    {
-
-
-    }
-
-    public function destroy(Request $request)
-    {
-
-
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,29 +8,15 @@ use \App\Http\Controllers\AccountController;
 
 
 Route::get('/', function () {
-    return 'UniVet ' . app()->version();
+    return 'PaPet ' . app()->version();
 });
 
 
 Route::middleware('cors')->group(function () {
 
-
-//    Route::controller(UsersController::class)->prefix('users')->group(function () {
-//        Route::get('/index', 'index');
-//        Route::get('/show', 'show');
-//        Route::post('/store', 'store');
-//        Route::put('/update', 'update');
-//        Route::delete('/delete/{id}', 'destroy');
-//    });
-//
-    Route::controller(VideosController::class)->prefix('videos')->group(function () {
-        Route::get('/index', 'index');
-        Route::get('/lives', 'lives');
-
-
-        Route::post('/show', 'show');
-        Route::post('/store', 'store');
-        Route::put('/update', 'update');
-        Route::delete('/delete/{id}', 'destroy');
+    Route::controller(UsersController::class)->prefix('users')->group(function () {
+        Route::get('/login', 'login');
+        Route::get('/register', 'register');
     });
+
 });
